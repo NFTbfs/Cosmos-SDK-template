b88c463b2fa89f3ed61914998c2689f5403d55a8
NFTbfs <rubydavis962@gmail.com>
2024-06-10 07:15:09 -0700

    update

---
+++ keeper/query_server.go
@@ -0,0 +1,76 @@
+package keeper
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"cosmossdk.io/collections"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/cosmosregistry/example"
+)
+
+var _ example.QueryServer = queryServer{}
+
+// NewQueryServerImpl returns an implementation of the module QueryServer.
+func NewQueryServerImpl(k Keeper) example.QueryServer {
+	return queryServer{k}
+}
+
+type queryServer struct {
+	k Keeper
+}
+
+// Counter defines the handler for the Query/Counter RPC method.
+func (qs queryServer) Counter(ctx context.Context, req *example.QueryCounterRequest) (*example.QueryCounterResponse, error) {
+	if _, err := qs.k.addressCodec.StringToBytes(req.Address); err != nil {
+		return nil, fmt.Errorf("invalid sender address: %w", err)
+	}
+
+	counter, err := qs.k.Counter.Get(ctx, req.Address)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return &example.QueryCounterResponse{Counter: 0}, nil
+		}
+
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &example.QueryCounterResponse{Counter: counter}, nil
+}
+
+// Counters defines the handler for the Query/Counters RPC method.
+func (qs queryServer) Counters(ctx context.Context, req *example.QueryCountersRequest) (*example.QueryCountersResponse, error) {
+	counters, pageRes, err := query.CollectionPaginate(
+		ctx,
+		qs.k.Counter,
+		req.Pagination,
+		func(key string, value uint64) (*example.Counter, error) {
+			return &example.Counter{
+				Address: key,
+				Count:   value,
+			}, nil
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	return &example.QueryCountersResponse{Counters: counters, Pagination: pageRes}, nil
+}
+
+// Params defines the handler for the Query/Params RPC method.
+func (qs queryServer) Params(ctx context.Context, req *example.QueryParamsRequest) (*example.QueryParamsResponse, error) {
+	params, err := qs.k.Params.Get(ctx)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return &example.QueryParamsResponse{Params: example.Params{}}, nil
+		}
+
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &example.QueryParamsResponse{Params: params}, nil
+}
---
+++ scripts/rename.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# This script is used to rename the example repo to the module name of your choice.
+# Usage MODULE_NAME=username/repo ./scripts/rename.sh
+# Disclaimer: this script do not work with multi-slash module names (e.g. my/awesome/repo) or module names with dashes (e.g. my-awesome-repo)
+# If you want to use a multi-slash module name, you will need to manually rename the module name in go.mod and the proto files.
+# A typical module name is 'username/proto' omitting the 'github.com/' prefix.
+
+if [[ -z $MODULE_NAME ]]; then
+    echo "MODULE_NAME must be set."
+    exit 1
+fi
+
+IFS='/' read -ra PARTS <<< "$MODULE_NAME"
+USERNAME="${PARTS[0]}"
+REPO="${PARTS[1]}"
+
+# remove all generated proto files
+find . -type f -name "*.pb.go" -delete
+find . -type f -name "*.pb.gw.go" -delete
+find . -type f -name "*.pulsar.go" -delete
+
+# rename module and imports
+go mod edit -module github.com/$MODULE_NAME
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    find . -not -path './.*' -type f -exec sed -i -e "s,cosmosregistry/example,$MODULE_NAME,g" {} \;
+    find . -name '*.proto' -type f -exec sed -i -e "s,cosmosregistry.example,$(echo "$MODULE_NAME" | tr '/' '.'),g" {} \;
+    find . -name 'protocgen.sh' -type f -exec sed -i -e "s,rm -rf github.com cosmosregistry,rm -rf github.com $USERNAME,g" {} \;
+    find . -not -path './.*' -type f -exec sed -i -e "s,example,$REPO,g" {} \;
+else
+    find . -not -path './.*' -type f -exec sed -i '' -e "s,cosmosregistry/example,$MODULE_NAME,g" {} \;
+    find . -name '*.proto' -type f -exec sed -i '' -e "s,cosmosregistry.example,$(echo "$MODULE_NAME" | tr '/' '.'),g" {} \;
+    find . -name 'protocgen.sh' -type f -exec sed -i '' -e "s,rm -rf github.com cosmosregistry,rm -rf github.com $USERNAME,g" {} \;
+    find . -not -path './.*' -type f -exec sed -i '' -e "s,example,$REPO,g" {} \;
+fi
+
+# rename directory
+mkdir -p proto/$MODULE_NAME
+mv proto/cosmosregistry/example/* proto/$MODULE_NAME
+rm -rf proto/cosmosregistry
+
+# re-generate protos
+make proto-gen
+
+# credits
+echo "# This Cosmos SDK module was generated using <https://github.com/cosmosregistry/example>" > THANKS.md
+
+# removes itself
+rm scripts/rename.sh
